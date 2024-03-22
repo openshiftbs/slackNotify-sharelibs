cb07877dc988f36ec0b44c8b0550cfb597f1a33c
openshiftbs <157187334+openshiftbs@users.noreply.github.com>
2024-03-22 18:44:11 +0530

    Update sendSlackNotification.groovy

--- vars/sendSlackNotification.groovy
+++ vars/sendSlackNotification.groovy
@@ -1,26 +1,28 @@
 def call(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
-  buildStatus =  buildStatus ?: 'SUCCESS'
+    // Checking if buildStatus is null or empty, defaulting to 'SUCCESS' if so
+    buildStatus = buildStatus ?: 'SUCCESS'
 
-  // Default values
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
+    // Default values
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    colorName = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
-    colorName = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    colorName = 'RED'
-    colorCode = '#FF0000'
-  }
+    // Override default values based on build status
+    switch (buildStatus) {
+        case 'STARTED':
+            colorName = 'YELLOW'
+            colorCode = '#FFFF00'
+            break
+        case 'SUCCESS':
+            colorName = 'GREEN'
+            colorCode = '#00FF00'
+            break
+        default:
+            colorName = 'RED'
+            colorCode = '#FF0000'
+    }
 
-  // Calling the slackSend function to Send notifications.
-  slackSend (color: colorCode, message: summary)
+    // Calling the slackSend function to Send notifications.
+    slackSend(color: colorCode, message: summary)
 }
